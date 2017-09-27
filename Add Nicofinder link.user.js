fc4f7ba063869badbce4f108c0a5b10f30a065e2
nico-noname <32145986+nico-noname@users.noreply.github.com>
2017-09-28 05:08:35 +0900

    Update Add Nicofinder link.user.js

--- Add Nicofinder link.user.js
+++ Add Nicofinder link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Add Nicofinder link Userscript
 // @namespace    https://github.com/nico-noname
-// @version      0.1
+// @version      0.11
 // @description  Add Nicofinder link
 // @author       nico-noname
 // @match        *://www.nicovideo.jp/*
@@ -13,8 +13,10 @@
 (function() {
     'use strict';
 
-      $("li[data-id]").each(function(i) {
-        var dataid = $(this).attr("data-id");
-        $(this).find("li.count.comment").children().wrapInner("<a href='http://www.nicofinder.net/comment/"+dataid+"' target='_new'></a>");
-      });
+    const Nicof ="http://www.nicofinder.net/comment/";
+
+    $("li[data-id]").each(function(i) {
+        let dataid = $(this).attr("data-id");
+        $(this).find("li.count.comment").children().wrapInner("<a></a>").children().attr("href",Nicof+dataid).attr("target","_new");
+    });
 })();
