ad0a414582e836c29e1c39f4f5a24200b7e58518
nico-noname <32145986+nico-noname@users.noreply.github.com>
2017-09-28 05:30:17 +0900

    Create AddNicofinderLink.user.js

---
+++ AddNicofinderLink.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Add Nicofinder Link
+// @namespace    https://github.com/nico-noname
+// @version      0.01
+// @description  Add Nicofinder Link
+// @author       nico-noname
+// @match        *://www.nicovideo.jp/*
+// @exclude      *://www.nicovideo.jp/mylist/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const Nicof ="http://www.nicofinder.net/comment/";
+
+    $("li[data-id]").each(function(i) {
+        let dataid = $(this).attr("data-id");
+        $(this).find("li.count.comment").children().wrapInner("<a></a>").children().attr({"href":Nicof+dataid,"target":"_new"});
+    });
+})();
